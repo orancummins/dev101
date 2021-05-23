5d05eeda2249ad9575e4a0b2212fde5114884f9e
orancummins <oran.cummins@gmail.com>
2021-05-23 15:43:35 +0100

    first commit

---
+++ src/test/java/com/oran/dev101/Dev101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oran.dev101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
